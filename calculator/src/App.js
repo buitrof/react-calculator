ef6bc2339dbeff8d69c08ff1e359a064457cb00b
fbuitron <franciscobuitron21@gmail.com>
2020-03-18 13:02:14 -0700

    added operator buttons and functions

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-    </div>
-  )
+class App extends Component {
+
+  state = {
+    firstNum: 0,
+    secondNum: 0,
+    result: 0
+  }
+
+  handleAddition = () => {
+    this.setState({ result: this.state.firstNum + this.state.secondNum })
+  }
+
+  handleSubtraction = () => {
+    this.setState({ result: this.state.firstNum - this.state.secondNum })
+  }
+
+  handleMultiplication = () => {
+    this.setState({ result: this.state.firstNum * this.state.secondNum })
+  }
+
+  handleDivision = () => {
+    this.setState({ result: this.state.firstNum / this.state.secondNum })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleAddition}>+</button>
+        <button onClick={this.handleSubtraction}>-</button>
+        <button onClick={this.handleMultiplication}>*</button>
+        <button onClick={this.handleDivision}>/</button>
+      </div>  
+    )
+  }
 }
 
 export default App
