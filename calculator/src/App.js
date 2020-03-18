c284b0776532a040e003e00dce6d5d29990fd573
pinheadrarry <richarddinh@gmail.com>
2020-03-18 13:31:25 -0700

    props

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -11,21 +11,7 @@ class App extends Component{
     operator: null,
     isFirstNumber: true,
   }
-  handleAddition = () => {
-    this.setState({ result: this.state.firstNum + this.state.secondNum })
-  }
 
-  handleSubtraction = () => {
-    this.setState({ result: this.state.firstNum - this.state.secondNum })
-  }
-
-  handleMultiplication = () => {
-    this.setState({ result: this.state.firstNum * this.state.secondNum })
-  }
-
-  handleDivision = () => {
-    this.setState({ result: this.state.firstNum / this.state.secondNum })
-  }
   render() {
     return (
       <>
@@ -39,12 +25,14 @@ class App extends Component{
             second_number = {this.state.secondNum}
             operator = {this.state.operator}
             isFirstNumber = {this.state.isFirstNumber}
-            restul = {this.state.result}
+            result = {this.state.result}
             />
           </div>
           <div className="col-lg-6">
             {/* Result Card */}
-            <Result />
+            <Result 
+            result = {this.state.result}
+            />
           </div>
         </div>
       </div>
--- calculator/src/components/Calculator/Calculator.js
+++ calculator/src/components/Calculator/Calculator.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react'
 
 class Calculator extends Component {
+  handleAddition = () => {
+    this.setState({ result: this.state.firstNum + this.state.secondNum })
+  }
+
+  handleSubtraction = () => {
+    this.setState({ result: this.state.firstNum - this.state.secondNum })
+  }
+
+  handleMultiplication = () => {
+    this.setState({ result: this.state.firstNum * this.state.secondNum })
+  }
+
+  handleDivision = () => {
+    this.setState({ result: this.state.firstNum / this.state.secondNum })
+  }
   render() {
+    console.log(this.props.first_number)
     return (
       <div className="card">
         <h3 className="card-header">Calculator</h3>
