952c9a39a6cd66f201be96bd134b12df94cdc4eb
pinheadrarry <richarddinh@gmail.com>
2020-03-18 13:17:57 -0700

    created calculator component

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,6 +4,28 @@ import Result from './components/Result'
 import Calculator from './components/Calculator'
 
 class App extends Component{
+  state = {
+    firstNum: 0,
+    secondNum: 0,
+    result: 0,
+    operator: null,
+    isFirstNumber: true,
+  }
+  handleAddition = () => {
+    this.setState({ result: this.state.firstNum + this.state.secondNum })
+  }
+
+  handleSubtraction = () => {
+    this.setState({ result: this.state.firstNum - this.state.secondNum })
+  }
+
+  handleMultiplication = () => {
+    this.setState({ result: this.state.firstNum * this.state.secondNum })
+  }
+
+  handleDivision = () => {
+    this.setState({ result: this.state.firstNum / this.state.secondNum })
+  }
   render() {
     return (
       <>
@@ -12,7 +34,13 @@ class App extends Component{
         <div className="row">
           <div className="col-lg-4">
             {/* Calulator Card Here */}
-            <Calculator />
+            <Calculator 
+            first_number = {this.state.firstNum}
+            second_number = {this.state.secondNum}
+            operator = {this.state.operator}
+            isFirstNumber = {this.state.isFirstNumber}
+            restul = {this.state.result}
+            />
           </div>
           <div className="col-lg-6">
             {/* Result Card */}
