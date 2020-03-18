fdd60e06fc17ecd6f53eed8633d9df978dab5470
Bryan Y <xpreyx@gmail.com>
2020-03-18 13:09:03 -0700

    Merge branch 'master'

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,24 +1,37 @@
-import React, {Component} from 'react'
-import Jumbotron from './components/Jumbotron'
-import Result from './components/Result'
+import React, { Component } from 'react'
 
-class App extends Component{
+class App extends Component {
+
+  state = {
+    firstNum: 0,
+    secondNum: 0,
+    result: 0
+  }
+
+  handleAddition = () => {
+    this.setState({ result: this.state.firstNum + this.state.secondNum })
+  }
+
+  handleSubtraction = () => {
+    this.setState({ result: this.state.firstNum - this.state.secondNum })
+  }
+
+  handleMultiplication = () => {
+    this.setState({ result: this.state.firstNum * this.state.secondNum })
+  }
+
+  handleDivision = () => {
+    this.setState({ result: this.state.firstNum / this.state.secondNum })
+  }
+
   render() {
     return (
-      <>
-      <Jumbotron />
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4">
-            {/* Calulator Card Here */}
-          </div>
-          <div className="col-lg-6">
-            {/* Result Card */}
-            <Result />
-          </div>
-        </div>
-      </div>
-      </>
+      <div>
+        <button onClick={this.handleAddition}>+</button>
+        <button onClick={this.handleSubtraction}>-</button>
+        <button onClick={this.handleMultiplication}>*</button>
+        <button onClick={this.handleDivision}>/</button>
+      </div>  
     )
   }
 }
